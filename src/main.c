0fc20bd1de41f90b15014763be6a264c7563a61b
이정우 <jwlee1130@naver.com>
2020-11-07 08:28:42 +0000

    푸는중

--- src/main.c
+++ src/main.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
 
-int main(){
-	printf("hellow world\n");
+void right_rotate(int arr[],int k){
+	
+	
+	
 }
+
+
+int main(){
+	int k;
+	int arr[]={1,2,3,4,5,6,7,8,9};
+	printf("몇만큼 오른쪽으로 회전할 것인가\n");
+	scanf("%d",&k);
+	
+	printf("%d\n",arr[k]);
+	
+}
+
